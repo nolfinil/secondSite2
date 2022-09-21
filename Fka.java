38dfe632afbc9688cc984fd2e8001fb4397dfa72
Ilya <113427959+nolfinil@users.noreply.github.com>
2022-09-21 10:41:27 +0600

    Add files via upload

---
+++ Fka.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Fka {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String password = scanner.nextLine();
+        if (password.length()>8 ){
+
+
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int c = scanner.nextInt();
+//        int d = scanner.nextInt();
+//        if (a<9 && a>0 && d<9 && d>0 && b<9 && b>0 && c<9 && c>0){
+//
+//            System.out.println("YES");
+//            System.out.println("NO");
+        }
+    }
+}
